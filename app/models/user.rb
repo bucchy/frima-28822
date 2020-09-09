8631be737c479f898c932ec81272867c4daa67eb
bucchy <bucchy0406@gmail.com>
2020-09-09 11:31:05 +0900

    テストコード修正1

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,17 @@ class User < ApplicationRecord
          :recoverable,
          :rememberable, 
          :validatable
-         
+
          has_many :puchases
          has_many :items
 
          validates :nickname, presence: true, length: { maximum: 6 }
+         validates :email, presence: true
+         validates :password, presence: true
+         validates :family_name, presence: true
+         validates :first_name, presence: true
+         validates :family_name_kana, presence: true
+         validates :first_name_kana, presence: true
+         validates :birthday, presence: true
 end
 
