9935e411792b06927e589e6ea7e9d7425cd5ef9f
bucchy <bucchy0406@gmail.com>
2020-09-11 15:08:36 +0900

    gitignore実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  
-  get 'articles/index'
-  get 'articles/new'
+  root to: "items#index"
+  # get 'articles/index'
+  # get 'articles/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'items#index'
-  resources :articles
+  # resources :articles
 end
