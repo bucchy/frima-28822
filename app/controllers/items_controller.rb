10f590a9ec1f2bf7ac08e24aab3ee2deb6f21691
bucchy <bucchy0406@gmail.com>
2020-09-17 12:43:05 +0900

    消費税導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  #before_action: :redirect_top only:[:new, :create]
+  before_action :authenticate_user!, only:[:new, :create]
 
   def index
     @item = Item.order("created_at DESC")
@@ -9,32 +9,24 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
   private
 
-   def redirect_top
-    unless user_signed_in? 
-      redirect_to root_path
-     end
-
-    end
-
-    def create
-      @item = Item.new(item_params)
-      if @item.save
-        redirect_to root_path
-      else
-        render :new
-      end
-    end
-  
-    private
-  
-    #３つのコードを１つにしてpriceを追加
-    def item_params
-      params.require(:item).permit(:title, :text, :genre_id, :condition_id, :shippingfee_id, :price)
-      params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
-    end
+  #３つのコードを１つにしてpriceを追加
+  def item_params
+    params.require(:item).permit(:title, :text, :genre_id, :condition_id, :shippingfee_id, :price)
+    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+  end
 
     
 
-  end
+end
---
+++ app/javascript/packs/fee.js
@@ -0,0 +1,19 @@
+function fee() {
+  price = document.getElementById("item-price")
+  price.addEventListener("input", function(){
+  var inputPrice = document.getElementById("item-price").value;
+  var tax = inputPrice*0.1; 
+  document.getElementById("add-tax-price").innerHTML = tax
+  document.getElementById("profit").innerHTML = inputPrice * 110 / 100
+  })
+
+    
+} 
+
+window.addEventListener("load", fee);
+
+//１価格のフォームをidに紐付けてとってくる
+//2とってきたフォームを使ってフォームに入力した時にイベント発火させる
+//３価格のフォームに入力した値をとってくる＝価格のフォームのバリュー要素をとってくる
+//４とってきた価格を使って手数料と利益を計算
+//５計算結果を販売手数料と販売利益のビューにつける（innerHTML）
