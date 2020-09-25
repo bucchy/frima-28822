2b3029ccf7657b3c6d1e21e1d8782c6f81856e8e
bucchy <bucchy0406@gmail.com>
2020-09-25 21:21:23 +0900

    commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only:[:new, :create]
-  before_action :set_item, only:[:show]
+  before_action :authenticate_user!, only:[:new, :create, :edit]
+  before_action :set_item, only:[:show, :edit, :update]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -20,9 +20,20 @@ class ItemsController < ApplicationController
   end
 
 def show
-  
+
 end
 
+def edit
+
+end
+
+def update
+  if @item.update(item_params)
+    redirect_to root_path
+  else
+    render :show
+  end
+end
 
   private
 
